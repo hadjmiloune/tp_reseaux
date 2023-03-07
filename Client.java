2cb04d6596a4577ff16e46998fbfde3060d79195
hadjmiloune <69020400+hadjmiloune@users.noreply.github.com>
2023-03-07 23:56:29 +0100

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,21 +1,48 @@
-import java.io.InputStream;
-import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
+
+
+
+
 
 public class Client {
   public static void main(String[] args) throws Exception {
-    Socket cs = new Socket("127.0.0.1", 9999);
+    utilisateur uti1=new utilisateur("2", "123");
+    int port = 9999;
+    String host = "localhost";
 
-    ObjectOutputStream out = new ObjectOutputStream(cs.getOutputStream());
-    out.writeObject(new SubStr(args[0], args[1]));
-    out.flush();
+    Socket client = new Socket(host, port);
 
-    byte[] b = new byte[200];
-    InputStream in = cs.getInputStream();
+    ObjectInputStream inp = new ObjectInputStream(client.getInputStream());
+    ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
 
-    in.read(b);
+    
+    out.writeObject(uti1);
 
-    System.out.println(new String(b));
-    cs.close();
+    String oper = (String) inp.readObject();
+    System.out.println(oper);
+    Scanner scn = new Scanner(System.in);
+    out.writeObject(scn.next());
+    int A[][]={{1,3,4},{2,4,3},{3,4,5}};    
+    int B[][]={{1,3,4},{2,4,3},{1,2,4}};    
+
+     out.writeObject(A);
+    out.writeObject(B);
+    
+    scn.close();
+    // get result
+
+    int[][] R = (int[][]) inp.readObject();
+
+    for (int i = 0; i < R.length; i++) {
+      for (int j = 0; j < R[i].length; j++)
+        System.out.print(R[i][j] + " , ");
+      System.out.println();
+    }
+    // print result
+
   }
 }
